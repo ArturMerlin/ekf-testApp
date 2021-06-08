c6dc682d791867a25882ba289835a6a1c9d07003
Nikita Kasturya <nikitakasturya@gmail.com>
2021-06-08 21:31:07 +0300

    Класс-модель с обобщёнными параметрами человека(полностью соответствует модели ребёнка)

---
+++ lib/src/models/person.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'person.g.dart';
+
+@JsonSerializable()
+class Person {
+  /// Класс человека с общими параметрами. Полностью соответствует модели ребёнка сотрудника
+  String firstName;
+  String lastName;
+  String middleName;
+  DateTime birthday;
+
+  Person(this.firstName, this.lastName, this.middleName, this.birthday);
+
+  factory Person.fromJson(Map<String, dynamic> json) => _$PersonFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PersonToJson(this);
+}
+
+
---
+++ lib/src/models/person.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'person.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Person _$PersonFromJson(Map<String, dynamic> json) {
+  return Person(
+    json['firstName'] as String,
+    json['lastName'] as String,
+    json['middleName'] as String,
+    DateTime.parse(json['birthday'] as String),
+  );
+}
+
+Map<String, dynamic> _$PersonToJson(Person instance) => <String, dynamic>{
+      'firstName': instance.firstName,
+      'lastName': instance.lastName,
+      'middleName': instance.middleName,
+      'birthday': instance.birthday.toIso8601String(),
+    };
