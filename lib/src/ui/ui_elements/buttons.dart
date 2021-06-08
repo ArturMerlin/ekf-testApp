3b7c7999d805c24ab6189a3be0723f37561230c0
Nikita Kasturya <nikitakasturya@gmail.com>
2021-06-08 23:21:45 +0300

    Виджет кнопки

---
+++ lib/src/ui/ui_elements/buttons.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class UsualButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String text;
+
+  const UsualButton({required this.onTap, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.blue.withOpacity(0.5),
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: Colors.blue, width: 1),
+      ),
+      child: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: onTap,
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
